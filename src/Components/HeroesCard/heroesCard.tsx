005dd5b59688144511d347a125198dbaafe5c285
Arman Petrosyan <137907864+ArmPet006@users.noreply.github.com>
2024-12-18 19:57:04 +0400

    heroes card button commit

--- src/Components/HeroesCard/heroesCard.tsx
+++ src/Components/HeroesCard/heroesCard.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 
 type HeroesCardPropTypes = {
-  logo: string | undefined;
+  image: string | undefined;
   title: string;
   description: string;
 };
 
-const HeroesCard = ({ logo, title, description }: HeroesCardPropTypes) => {
+const HeroesCard = ({ image, title, description }: HeroesCardPropTypes) => {
   return (
     <div className="heroes_card">
       <div className="heroes_card_heading">
         <div className="heroes_card_logo"></div>
         <div className="heroes_card_title">{title}</div>
         <div className="heroes_card_icons">
-            <div className="heroes_card_icon"></div>
-            <div className="heroes_card_icon"></div>
-            <div className="heroes_card_icon"></div>
-            <div className="heroes_card_icon"></div>
+            <button className="heroes_card_icon"></button>
+            <button className="heroes_card_icon"></button>
+            <button className="heroes_card_icon"></button>
+            <button className="heroes_card_icon"></button>
         </div>
       </div>
-      <div className="heroes_card_description">{description}</div>
+      <div className="heroes_card_description">
+        <p>{description}</p>
+        <button className="heroes_card_button">Read More</button>
+      </div>
     </div>
   );
 };
--- src/Pages/Heroes/heroes.tsx
+++ src/Pages/Heroes/heroes.tsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import HeroesCard from '../../Components/HeroesCard/heroesCard'
+import React from "react";
+import HeroesCard from "../../Components/HeroesCard/heroesCard";
 
 const Heroes = () => {
+  const arr = Array(10).fill({
+    image: undefined,
+    title: "Heading",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?"
+  });
   return (
-    <div className='heroes_container'>
-      <HeroesCard logo={undefined} title={'Heading'} description={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?'}/>
-      <HeroesCard logo={undefined} title={'Heading'} description={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?'}/>
-      <HeroesCard logo={undefined} title={'Heading'} description={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?'}/>
-      <HeroesCard logo={undefined} title={'Heading'} description={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?'}/>
-      <HeroesCard logo={undefined} title={'Heading'} description={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?'}/>
-      <HeroesCard logo={undefined} title={'Heading'} description={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?'}/>
-      <HeroesCard logo={undefined} title={'Heading'} description={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?'}/>
-      <HeroesCard logo={undefined} title={'Heading'} description={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?'}/>
-      <HeroesCard logo={undefined} title={'Heading'} description={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?'}/>
-      <HeroesCard logo={undefined} title={'Heading'} description={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti voluptatibus velit sint obcaecati voluptatem sequi, suscipit deserunt! Maxime aliquam, est harum, fuga possimus quos saepe nam nostrum tempore a nemo?'}/>
+    <div className="heroes_container">
+      {arr.map((element, index) => (
+        <HeroesCard
+          key={index}
+          image={element.image}
+          title={element.title}
+          description={element.description}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Heroes
+export default Heroes;
