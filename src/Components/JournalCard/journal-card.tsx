6127bc3f1de83f1339aabb4ab1299281c98c80b3
Artur <petrosyanartur78@gmail.com>
2025-02-03 17:05:22 +0400

    fix: changed ts config for initial build

--- src/Components/JournalCard/journal-card.tsx
+++ src/Components/JournalCard/journal-card.tsx
@@ -1,27 +1,27 @@
-import React, { FC } from "react";
+import React, {FC} from "react";
 import Button from "../UI/Button/button";
 
 import "./journal-card.scss";
 
 interface Props {
-	image: string | undefined;
-	title: string;
-	description: string;
-};
+    image: string | undefined;
+    title: string;
+    description: string;
+}
 
-const JournalCard: FC<Props> = ({ image, title, description }) => {
-	return (
-		<div className="specialProject_card">
-			<div className="specProj_card_heading">
-				<div className="specProj_card_logo"></div>
-				<div className="heroes_card_title">{title}</div>
-				<Button>Read More</Button>
-			</div>
-			<div className="specProj_card_description">
-				<p>{description}</p>
-			</div>
-		</div>
-	);
+const JournalCard: FC<Props> = ({image, title, description}) => {
+    return (
+        <div className="specialProject_card">
+            <div className="specProj_card_heading">
+                <div className="specProj_card_logo"></div>
+                <div className="heroes_card_title">{title}</div>
+                {/*<Button>Read More</Button>*/}
+            </div>
+            <div className="specProj_card_description">
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 };
 
 export default JournalCard;
