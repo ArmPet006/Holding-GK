a472407f6cd4ddc256ddd12ca8c79305b1ac000f
Artur <petrosyanartur78@gmail.com>
2025-02-03 17:09:47 +0400

    fix: fixes for initial build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         <head>
             <title>Next app</title>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"}/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
             <link
                 href="https://fonts.googleapis.com/css2?family=Lovers+Quarrel&display=swap"
                 rel="stylesheet"
